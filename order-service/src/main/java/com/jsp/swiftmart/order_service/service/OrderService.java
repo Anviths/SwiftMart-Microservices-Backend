24be04da18cf052050f1785cd4950b2055a57939
anvith <anwithgowda2@gmail.com>
2026-04-07 22:51:43 +0530

    order

--- order-service/src/main/java/com/jsp/swiftmart/order_service/service/OrderService.java
+++ order-service/src/main/java/com/jsp/swiftmart/order_service/service/OrderService.java
@@ -2,9 +2,11 @@ package com.jsp.swiftmart.order_service.service;
 
 import com.jsp.swiftmart.order_service.dto.OrderRequest;
 import com.jsp.swiftmart.order_service.dto.OrderResponse;
+import org.springframework.data.domain.Pageable;
 
 public interface OrderService {
 
-    OrderResponse createOrder(OrderRequest orderRequest);
-    OrderResponse
+    OrderResponse createOrder(Long useId);
+    OrderResponse findOrderByOrderId(Long userId,Long orderId);
+    Pageable findAllOrder(Long userId,int pageNo,int size);
 }
